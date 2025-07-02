1f21ba57fe7fe1807e0ab41e0ee880df99dfff31
CassBo <cassiel.hernandez@tecdesoftware.edu.mx>
2025-07-02 08:58:38 -0600

    MapStruct Implemented

--- src/main/java/com/tecdesoftware/market/persistance/ProductoRepository.java
+++ src/main/java/com/tecdesoftware/market/persistance/ProductoRepository.java
@@ -2,14 +2,39 @@ package com.tecdesoftware.market.persistance;
 
 import com.tecdesoftware.market.persistance.crud.ProductoCrudRepository;
 import com.tecdesoftware.market.persistance.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository  productoCrudRepository;
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
         }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto saveProducto(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+
 }
 
